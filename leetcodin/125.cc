e66e6972ab7b3b3609ec47d35bbe6fe9ae04e181
Dave <davidkhauss@gmail.com>
2024-07-17 13:59:52 +0000

    final implementation, style cleaned up

--- leetcodin/125.cc
+++ leetcodin/125.cc
@@ -10,7 +10,8 @@ the same forward and backward. Alphanumeric characters include letters
 and numbers.
 */
 
-bool isPalindrome(std::string& s);
+bool is_palindrome_final(const std::string& s);
+bool is_palindrome(std::string& s);
 uint32_t strip_and_lower(std::string& s);
 
 int main()
@@ -19,15 +20,50 @@ int main()
     auto test_2 = std::string{"race a car"};
     auto test_3 = std::string{" "};
 
-    std::cout << std::boolalpha << isPalindrome(test_1) << '\n';
-    std::cout << std::boolalpha << isPalindrome(test_2) << '\n';
-    std::cout << std::boolalpha << isPalindrome(test_3) << '\n';
+    std::cout << std::boolalpha << is_palindrome_final(test_1) << '\n';
+    std::cout << std::boolalpha << is_palindrome_final(test_2) << '\n';
+    std::cout << std::boolalpha << is_palindrome_final(test_3) << '\n';
+
+    std::cout << std::boolalpha << is_palindrome(test_1) << '\n';
+    std::cout << std::boolalpha << is_palindrome(test_2) << '\n';
+    std::cout << std::boolalpha << is_palindrome(test_3) << '\n';
 
     return 0;
 }
 
-bool isPalindrome(std::string& s) 
+bool is_palindrome_final(const std::string& s)
+    // checks if a string is a palindrome in place without altering original string
 {
+    std::int32_t l = 0;
+    std::int32_t r = s.size() - 1;
+
+    while(l < r)
+    {
+        while(!isalnum(s[l]))
+        {
+            ++l;
+        }
+
+        while(!isalnum(s[r]))
+        {
+            --r;
+        }
+
+        if(tolower(s[l]) != tolower(s[r]))
+        {
+            return false;
+        }
+
+        ++l;
+        --r;
+    }
+
+    return true;
+}
+
+bool is_palindrome(std::string& s) 
+{
+    // checks if string is a palindrome but alters original string
     std::int32_t l = 0;
     std::int32_t r = strip_and_lower(s) - 1;
 
